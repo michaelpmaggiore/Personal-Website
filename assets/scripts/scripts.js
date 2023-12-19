da34bcf606f97135899ee0a068da6f04a51b1e8a
Michael Maggiore <michaelpmaggiore@gmail.com>
2023-12-19 10:55:25 -0700

    Added portrait of photos to front page

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -25,11 +25,15 @@ typewriter.typeString('Hey there!') // Maybe capitalize the sentences?!
     .pauseFor(1000)
     .deleteAll()
     .pauseFor(500)
-    .typeString("I'm studying computer science with a focus in Robotics and AI")
+    .typeString("I'm studying computer science")
     .pauseFor(1000)
     .deleteAll()
     .pauseFor(500)
-    .typeString("I'm also interested in software engineering")
+    .typeString("I'm interested in DevOps and Full-Stack development")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("I also enjoy software engineering!")
     .pauseFor(1000)
     .deleteAll()
     .pauseFor(500)
@@ -37,7 +41,7 @@ typewriter.typeString('Hey there!') // Maybe capitalize the sentences?!
     .pauseFor(1000)
     .deleteAll()
     .pauseFor(500)
-    .typeString("I also like to swim")
+    .typeString("waterpolo > competitive swimming")
     .pauseFor(1000)
     .deleteAll()
     .pauseFor(500)
@@ -136,3 +140,58 @@ for (var i = 0; i < btns.length; i++) {
 // // sr.reveal('.experience', { viewFactor: 0.2 });
 // // sr.reveal('.featured-projects', { viewFactor: 0.1 });
 // sr.reveal('.project-section', { viewFactor: 0.05 });
+
+
+startImageTransition();
+        
+function startImageTransition() {
+    var images = document.getElementsByClassName("test");
+
+    for (var i = 0; i < images.length; ++i) {
+        images[i].style.opacity = 1;
+    }
+
+    var top = 1;
+
+    var cur = images.length - 1;
+
+    setInterval(changeImage, 5000);
+
+    async function changeImage() {
+
+        var nextImage = (1 + cur) % images.length;
+
+        images[cur].style.zIndex = top + 1;
+        images[nextImage].style.zIndex = top;
+
+        await transition();
+
+        images[cur].style.zIndex = top;
+
+        images[nextImage].style.zIndex = top + 1;
+
+        top = top + 1;
+
+        images[cur].style.opacity = 1;
+      
+        cur = nextImage;
+
+    }
+
+    function transition() {
+        return new Promise(function(resolve, reject) {
+            var del = 0.01;
+
+            var id = setInterval(changeOpacity, 10);
+
+            function changeOpacity() {
+                images[cur].style.opacity -= del;
+                if (images[cur].style.opacity <= 0) {
+                    clearInterval(id);
+                    resolve();
+                }
+            }
+
+        })
+    }
+  }
