a1a2b73d77adda2d7a22e81918ff344d1a1c0612
Michael Maggiore <michaelpmaggiore@gmail.com>
2023-12-21 02:41:41 -0700

    Refactored css, index.html, and scripts.js by making everything look cleaner. Removed unecessary code/comments

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,6 +1,5 @@
-// Typewriter created by Tameem Safi.
+// Typewriter effect created by Tameem Safi.
 // Source: https://github.com/tameemsafi/typewriterjs
-
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
@@ -50,6 +49,7 @@ typewriter.typeString('Hey there!')
     .deleteAll()
     .start();
 
+// If user clicks on 'Michael Maggiore' then take user to home page and they should see 'Thanks for visiting' in text.
 document.getElementById('toHomeSection').addEventListener('click', function (event) {
   typewriter.stop();
   typewriter = null;
@@ -60,6 +60,7 @@ document.getElementById('toHomeSection').addEventListener('click', function (eve
       delay: 50,
       deleteSpeed: 5
   });
+
   typewriter.typeString("Thanks for visiting!")
   .pauseFor(1000)
   .deleteAll()
@@ -102,61 +103,63 @@ document.getElementById('toHomeSection').addEventListener('click', function (eve
   .start();
 })
 
-  // Start animation for intro page.
-  window.onload = () => {
-    var element = document.getElementById("fade-in-element0");
-    setTimeout(() => { animateFadeIn(element, 600); }, 100);
-  }
-
-
-  window.onscroll = function () {
-    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-      document.getElementById("navbar").style.top = "0";
-    } else {
-      document.getElementById("navbar").style.top = "-70px";
-    }
-
-    scrollFunction();
-  };
-
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-function scrollFunction() {
-  fadeInElement("about-section-description", 700, 0, 200);
-  fadeInElement("fade-in-element0.5", 700, 0, 400);
-  fadeInElement("fade-in-element0.7", 700, 0, 200);
-
-  fadeInElement("fade-in-element2.1", 1400, 0, 200);
-  fadeInElement("fade-in-element2.2", 1400, 0, 400);
-  fadeInElement("school", 1400, 0, 400);
-  fadeInElement("coursework", 1400, 0, 600);
-
-  fadeInElement("fade-in-element1", 2500, 1, 200);
-  // fadeInElement("button-section", 2500, 2, 200);
-
-  fadeInElement("fade-in-element2", 2800, 3, 200);
-  fadeInElement("fade-in-element3", 2800, 3, 400);
-  fadeInElement("fade-in-element4", 2800, 3, 600);
-
-  fadeInElement("fade-in-element5", 3100, 3, 800);
-  fadeInElement("fade-in-element6", 3100, 3, 1000);
-  fadeInElement("fade-in-element7", 3100, 3, 1200);
-
-  fadeInElement("fade-in-element8", 3400, 3, 800);
-  fadeInElement("fade-in-element9", 3400, 3, 1000);
-  fadeInElement("fade-in-element10", 3400, 3, 1200);
-
-
+// Start fade-in animation for home page.
+window.onload = () => {
+  var element = document.getElementById("home-fade");
+  setTimeout(() => { animateFadeIn(element, 600); }, 100);
 }
 
-function fadeInElement(elementId, scrollThreshold, zIndex, duration) {
+// When user scrolls to a certain point, activate fade-in effects for each important page element.
+window.onscroll = function () {
+
+  // When the user scrolls down 20px from the top of the document, slide down the navbar
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-70px";
+  }
+
+  scrollFunction();
+};
+
+function scrollFunction() {
+
+  // Handles About page
+  fadeInElement("about-desc", 700, 200);
+  fadeInElement("about-title", 700, 400);
+  fadeInElement("about-image-container", 700, 200);
+
+  // Handles Education page
+  fadeInElement("education-title", 1400, 200);
+  fadeInElement("education-college-container", 1400, 400);
+  fadeInElement("coursework", 1400, 600);
+
+  // Handles Projects page
+  fadeInElement("projects-title", 2500, 200);
+
+  fadeInElement("project-webmd", 2800, 200);
+  fadeInElement("project-keyboard", 2800, 400);
+  fadeInElement("project-chess", 2800, 600);
+
+  fadeInElement("project-svm", 3100, 800);
+  fadeInElement("project-budgetBuddy", 3100, 1000);
+  fadeInElement("project-clueGame", 3100, 1200);
+
+  fadeInElement("project-fti", 3400, 800);
+  fadeInElement("project-ucs", 3400, 1000);
+  fadeInElement("project-portfolio", 3400, 1200);
+}
+
+// Logic to determine scroll threshold for fade-ins
+function fadeInElement(elementId, scrollThreshold, duration) {
   var element = document.getElementById(elementId);
 
   if ((document.body.scrollTop > scrollThreshold || document.documentElement.scrollTop > scrollThreshold)) {
-    // element.style.zIndex = zIndex + 1;
     animateFadeIn(element, duration);
   }
 }
 
+// Logic to determine animation fade-ins
 function animateFadeIn(element, duration) {
   var startTime = null;
   var startOpacity = parseFloat(element.style.opacity) || 0;
@@ -175,22 +178,15 @@ function animateFadeIn(element, duration) {
   requestAnimationFrame(step);
 }
 
-
-//filter buttons 
-//source : https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_elements
-
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("each-project");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    w3AddClass(x[i], "show");
-  }
+// Allows projects to show up in their individual boxes.
+var x, i;
+x = document.getElementsByClassName("each-project");
+for (i = 0; i < x.length; i++) {
+  w3AddClass(x[i], "show");
 }
 
-x = document.getElementsByClassName("each-project2");
+// Pops up about-image.
+x = document.getElementsByClassName("about-image-container");
 w3AddClass(x[0], "show")
 
 function w3AddClass(element, name) {
@@ -201,97 +197,3 @@ function w3AddClass(element, name) {
     if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
   }
 }
-
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-// var btnContainer = document.getElementById("button-section");
-// var btns = btnContainer.getElementsByClassName("filter-button");
-// for (var i = 0; i < btns.length; i++) {
-//   btns[i].addEventListener("click", function(){
-//     var current = document.getElementsByClassName("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//     console.log(current);
-//   });
-// }
-
-// // Reveals website.
-// window.sr = ScrollReveal({
-//   reset: false,
-//   duration: 600,
-//   easing: 'cubic-bezier(.694,0,.335,1)',
-//   scale: 1,
-//   viewFactor: 0.3,
-// });
-
-// sr.reveal('.home-section');
-// sr.reveal('.about-section');
-// // sr.reveal('.experience', { viewFactor: 0.2 });
-// // sr.reveal('.featured-projects', { viewFactor: 0.1 });
-// sr.reveal('.project-section', { viewFactor: 0.05 });
-
-
-startImageTransition();
-        
-function startImageTransition() {
-    var images = document.getElementsByClassName("test");
-
-    for (var i = 0; i < images.length; ++i) {
-        images[i].style.opacity = 1;
-    }
-
-    var top = 1;
-
-    var cur = images.length - 1;
-
-    setInterval(changeImage, 5000);
-
-    async function changeImage() {
-
-        var nextImage = (1 + cur) % images.length;
-
-        images[cur].style.zIndex = top + 1;
-        images[nextImage].style.zIndex = top;
-
-        await transition();
-
-        images[cur].style.zIndex = top;
-
-        images[nextImage].style.zIndex = top + 1;
-
-        top = top + 1;
-
-        images[cur].style.opacity = 1;
-      
-        cur = nextImage;
-
-    }
-
-    function transition() {
-        return new Promise(function(resolve, reject) {
-            var del = 0.01;
-
-            var id = setInterval(changeOpacity, 10);
-
-            function changeOpacity() {
-                images[cur].style.opacity -= del;
-                if (images[cur].style.opacity <= 0) {
-                    clearInterval(id);
-                    resolve();
-                }
-            }
-
-        })
-    }
-  }
